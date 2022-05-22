9472c00eafc00c2af59df33dc365a135e9bbd4f6
fhenseler <fhenseler@outlook.com>
2022-05-22 00:55:12 -0300

    fix: links

--- env-config.js
+++ env-config.js
@@ -1,4 +1,5 @@
 const prod = process.env.NODE_ENV === 'production'
 module.exports = {
-    'process.env.BACKEND_URL': prod ? '/Next-gh-page-example' : ''
+    basePath: '/portfolio',
+    'process.env.BACKEND_URL': prod ? '/portfolio' : ''
   }
